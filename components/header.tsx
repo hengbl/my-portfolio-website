62067a9139e3fafc1b82c7ce2b2c3589bcefe107
Heng Boon Long <pngwenlong@gmail.com>
2024-05-02 23:46:50 +0800

    Change header colour

--- components/header.tsx
+++ components/header.tsx
@@ -11,14 +11,12 @@ const robotoMono = Roboto_Mono({ weight:"400", subsets: ['latin'] });
 export default function Header() {
   return <header className="z-[999] relative">
 
-    <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40
-    bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem]
-    sm:top-6 sm:h-[3.25rem] sm:w-[38rem] sm:rounded-full"
-        
-        /* Add a bounce animation on the navbar when page loads using primer-motion */
+    <motion.div 
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40
+        bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem]
+        sm:top-6 sm:h-[3.25rem] sm:w-[38rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1}}
-
     >
     </motion.div>
 

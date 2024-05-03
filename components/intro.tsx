52ff16ea99808e6fe88eebd6fdcfd0dc70d5452e
Heng Boon Long <pngwenlong@gmail.com>
2024-05-03 12:28:49 +0800

    Added socials to intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,11 +3,22 @@
 import React from 'react';
 import Image from 'next/image';
 import { motion } from "framer-motion";
+import { Roboto_Mono } from 'next/font/google';
+import Link from 'next/link';
+import { BsArrowRight } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
+
+
+const robotoMono = Roboto_Mono({ weight:["200", "400","700"], subsets: ['latin'] });
 
 export default function Intro() {
   return (
-    <section>
-        <div className="flex items-center justify-center">
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+        <div className="flex-col flex items-center justify-center">
+
+            {/* Profile Image and Waving Emoji */}
             <div className="relative">
                 <motion.div
                     initial={{ opacity: 0, scale: 0 }}
@@ -20,7 +31,7 @@ export default function Intro() {
                     <Image src="/profile-pic.png" alt="Portrait" 
                     width="192" height="192" 
                     priority={true} 
-                    className="h-full w-full rounded-full object-cover border-[0.35rem] border-white shadow-xl" 
+                    className="h-100 w-100 rounded-full object-cover border-[0.35rem] border-white shadow-xl" 
                     />
                     <motion.span className="absolute bottom-0 right-0 text-4xl"
                         initial={{ opacity: 0, scale: 0}}
@@ -36,9 +47,49 @@ export default function Intro() {
                 </motion.div>
             </div>
 
-            <p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
-                <span className="font-bold">Hello, I'm Boon Long.</span> I enjoy building Data Science and Web Development projects.
-            </p>
+            {/* Introduction Message */}
+            <motion.p className={`${robotoMono.className} mb-10 mt-4 px-4 text-xl font-medium !leading-[1.5] sm:text-2xl`}
+            initial={{ opacity: 0, y: 100}}
+            animate={{ opacity: 1, y: 0 }}
+            >
+                <span className="font-bold">Hello, I'm Boon Long.</span> I enjoy building Data Science and Web Development projects. <span className="font-thin">|</span>
+            </motion.p>
+
+            <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-s font-medium"
+            initial={{ opacity: 0, y: 100}}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+                delay: 0.1,
+            }}
+            >
+                {/* Download CV */}
+                <a className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
+                focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer"
+                href="/CV.pdf" download={ true }>
+                    Download CV <HiDownload className="opacity-70 group-hover:translate-y-1 transition"/>
+                </a>
+
+                {/* Redirect to Contact Me section */}
+                <Link href="#contact" className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
+                focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10">
+                    Contact me here <BsArrowRight className="opacity-60 group-hover:translate-x-1 transition"/>
+                </Link>
+
+                {/* Redirect to Linkedin */}
+                <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none 
+                focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10 cursor-pointer"
+                href="https://www.linkedin.com/in/hengboonlong/" target="_blank">
+                    <BsLinkedin />
+                </a>
+
+                {/* Redirect to Github */}
+                <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] outline-none 
+                focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10 cursor-pointer"
+                href="https://github.com/hengbl" target="_blank">
+                    <FaGithubSquare />
+                </a>
+
+            </motion.div>
 
         </div>
     </section>

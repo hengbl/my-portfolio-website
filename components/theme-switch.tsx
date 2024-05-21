c3a1f9b5e4a453f01f077da7878558946d0566d6
Heng Boon Long <pngwenlong@gmail.com>
2024-05-21 12:59:16 +0800

    theme switch button

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -6,16 +6,22 @@ import { useTheme } from '@/context/theme-context';
 
 
 export default function ThemeSwitch() {
-
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button className="fixed bottom-5 left-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 
-    backdrop-blur-[0.5rem] border border-black/10 border-opacity-40 shadow-2xl rounded-full
-    flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all
-    dark:bg-gray-950"
-    onClick={toggleTheme}>
-        { theme === "light" ? <BsSun /> : <BsMoon /> }
-    </button>
+    <div className="fixed bottom-5 left-5">
+      <label className="relative inline-flex items-center cursor-pointer">
+        <input type="checkbox" className="sr-only" checked={theme === "dark"} onChange={toggleTheme} />
+        <div className={`relative w-16 h-8 rounded-full transition-colors duration-300 
+          ${theme === "dark" ? "bg-[#4D46BF] border-white border-2" : "bg-[#73BDBB] border-gray-200 border-2" }`}>
+          <div className={`absolute top-1/2 transform -translate-y-1/2 w-6 h-6 rounded-full shadow-md transition-transform duration-300 ease-in-out
+            ${theme === "dark" ? "translate-x-8 bg-[#7D79D3]" : "translate-x-1 bg-[#F8DC6E]"}`}>
+            <div className="flex items-center justify-center h-full text-gray-700 dark:text-yellow-300">
+              {theme === "light" ? <BsSun size={18} /> : <BsMoon size={18} />}
+            </div>
+          </div>
+        </div>
+      </label>
+    </div>
   );
 }
